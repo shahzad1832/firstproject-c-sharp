49a9ba62b5b0bfdf3f38709c676090799e4bd4f4
M shehzad 01 <03098646766bhai@gmail.com>
2026-05-23 18:15:27 +0500

    init

--- obj/Debug/net10.0/firstProject.AssemblyInfo.cs
+++ obj/Debug/net10.0/firstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ee69d24f31cee766a755d0ee7b875591570b1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c50bbeb91ad2797bcba96232d47c20cf517f392")]
 [assembly: System.Reflection.AssemblyProductAttribute("firstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
